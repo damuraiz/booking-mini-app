220860b61a04830bb007e6acbd7ea775918f0759
damuraiz <samurai.net@gmail.com>
2024-05-05 02:38:34 +0300

    alpha

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,9 @@ import {ru} from "date-fns/locale";
 
 function App() {
   return (
-      <div className="App" style={{width: '100%', padding: '20px'}}>
+      <div className="App" style={{width: '100%'}}>
           <h1 style={{textAlign: 'center'}}>Выбор диапазона дат</h1>
-          <div style={{margin: '0 auto', maxWidth: '520px'}}>
+          <div style={{margin: '0 auto'}}>
               <MyDateRangePicker locale={ru}/>
           </div>
       </div>
